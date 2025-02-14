e238b7f5909b2c31a7ca4cdfd5a18c049d74aafe
nassima_bouaraba <nassimabouaraba03@gmail.com>
2025-02-14 15:50:17 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,135 +1,121 @@
 #!/bin/bash
+# Script d'installation Arch Linux avec LUKS, LVM et une partition /boot séparée
 
-# S'assurer que le script est exécuté en tant que root
+# Vérification root
 if [ "$EUID" -ne 0 ]; then
-    echo "Script must be executed as root"
-    exit 1
+  echo "Veuillez exécuter ce script en tant que root."
+  exit 1
 fi
 
-# VARIABLES DE PARTITIONNEMENT
+# Définition du disque
 DISK="/dev/sda"
-HOSTNAME="arch-vm"
 
-# FICHIER LOG POUR VÉRIFIER LES INSTALLS
-LOGFILE="/var/log/install_script.log"
-exec > >(tee -a "$LOGFILE") 2>&1
+# Partitionnement
+parted --script $DISK mklabel gpt
+parted --script $DISK mkpart primary fat32 1MiB 513MiB
+parted --script $DISK set 1 esp on
+parted --script $DISK mkpart primary ext4 513MiB 1GiB
+parted --script $DISK mkpart primary 1GiB 100%
 
-setup_partitions() {
-    echo "=== Nettoyage des partitions existantes ==="
-    for mount in /mnt/share /mnt/vbox /mnt/boot /mnt; do
-        umount -f $mount 2>/dev/null || true
-    done
+# Cryptsetup
+echo -n "azerty123" | cryptsetup luksFormat --type luks2 ${DISK}3 -
+echo -n "azerty123" | cryptsetup open ${DISK}3 cryptroot
 
-    lvchange -an /dev/vg0/* 2>/dev/null || true
-    vgchange -an 2>/dev/null || true
-    vgremove -f vg0 2>/dev/null || true
-    pvremove -f /dev/mapper/cryptlvm 2>/dev/null || true
+# LVM
+pvcreate /dev/mapper/cryptroot
+vgcreate vg0 /dev/mapper/cryptroot
+lvcreate -L 30G -n root vg0
+lvcreate -L 8G -n swap vg0
+lvcreate -L 10G -n encrypted vg0
+lvcreate -L 5G -n shared vg0
+lvcreate -l 100%FREE -n home vg0
 
-    cryptsetup close cryptlvm 2>/dev/null || true
+# Formatage
+mkfs.fat -F32 ${DISK}1
+mkfs.ext4 ${DISK}2
+mkfs.ext4 /dev/vg0/root
+mkfs.ext4 /dev/vg0/home
+mkfs.ext4 /dev/vg0/shared
+mkswap /dev/vg0/swap
+swapon /dev/vg0/swap
 
-    partprobe $DISK
-    sleep 2
+# Partition chiffrée supplémentaire
+echo -n "azerty123" | cryptsetup luksFormat --type luks2 /dev/vg0/encrypted -
+echo -n "azerty123" | cryptsetup open /dev/vg0/encrypted encrypted_volume
+mkfs.ext4 /dev/mapper/encrypted_volume
+cryptsetup close encrypted_volume
 
-    echo "=== Effacement des partitions existantes ==="
-    dd if=/dev/zero of=$DISK bs=512 count=2048
-    wipefs -af $DISK
-    sgdisk -Z $DISK
-    partprobe $DISK
-    sleep 2
+# Montage
+mount /dev/vg0/root /mnt
+mkdir -p /mnt/boot
+mount ${DISK}2 /mnt/boot
+mkdir -p /mnt/efi
+mount ${DISK}1 /mnt/efi
+mkdir -p /mnt/home
+mount /dev/vg0/home /mnt/home
+mkdir -p /mnt/shared
+mount /dev/vg0/shared /mnt/shared
 
-    echo "=== Création des nouvelles partitions ==="
-    parted -s $DISK mklabel gpt
-    sgdisk -n 1:0:+512M -t 1:ef00 $DISK
-    sgdisk -n 2:0:0 -t 2:8e00 $DISK
-    partprobe $DISK
-    sleep 2
+# Installation base
+pacstrap /mnt base linux linux-firmware lvm2 vim networkmanager
 
-    mkfs.fat -F32 "${DISK}1"
+# Fstab
+genfstab -U /mnt >> /mnt/etc/fstab
 
-    echo -n "azerty123" | cryptsetup luksFormat "${DISK}2" -
-    echo -n "azerty123" | cryptsetup open "${DISK}2" cryptlvm -
-
-    pvcreate -ff /dev/mapper/cryptlvm
-    vgcreate vg0 /dev/mapper/cryptlvm
-
-    lvcreate -y -L 10G vg0 -n lvroot
-    lvcreate -y -L 10G vg0 -n lvvbox
-    lvcreate -y -L 5G vg0 -n lvshare
-
-    mkfs.ext4 -F /dev/vg0/lvroot
-    mkfs.ext4 -F /dev/vg0/lvvbox
-    mkfs.ext4 -F /dev/vg0/lvshare
-
-    mkdir -p /mnt /mnt/boot /mnt/vbox /mnt/share
-
-    mount /dev/vg0/lvroot /mnt || true
-    mount "${DISK}1" /mnt/boot || true
-    mount /dev/vg0/lvvbox /mnt/vbox || true
-    mount /dev/vg0/lvshare /mnt/share || true
-}
-
-setup_base_system() {
-    echo "=== Installation du système de base ==="
-    pacstrap /mnt base linux linux-firmware base-devel
-    genfstab -U /mnt >> /mnt/etc/fstab
-    echo "Base system installed, fstab generated."
-}
-
-setup_config() {
-echo "=== Configuration du système (chroot) ==="
-cat <<EOF | arch-chroot /mnt /bin/bash
+# Configuration système
+arch-chroot /mnt /bin/bash <<EOF
+# Région
 ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
 hwclock --systohc
 
-sed -i 's/^#fr_FR.UTF-8 UTF-8/fr_FR.UTF-8 UTF-8/' /etc/locale.gen
+# Langue
+echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
 locale-gen
-echo "LANG=fr_FR.UTF-8" > /etc/locale.conf
-echo "KEYMAP=fr" > /etc/vconsole.conf
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
 
-echo "$HOSTNAME" > /etc/hostname
-echo "127.0.0.1   localhost" >> /etc/hosts
-echo "::1         localhost" >> /etc/hosts
-echo "127.0.1.1   $HOSTNAME" >> /etc/hosts
+# Réseau
+echo "archbox" > /etc/hostname
+echo "127.0.0.1 localhost" >> /etc/hosts
+echo "::1       localhost" >> /etc/hosts
+echo "127.0.1.1 archbox.localdomain archbox" >> /etc/hosts
+systemctl enable NetworkManager
 
+# Utilisateurs
 echo "root:azerty123" | chpasswd
+useradd -m -G wheel colleague
+echo "colleague:azerty123" | chpasswd
+useradd -m son
+echo "son:azerty123" | chpasswd
 
-sed -i 's/^HOOKS=(base udev autodetect modconf block filesystems keyboard fsck)/HOOKS=(base udev autodetect modconf block encrypt lvm2 filesystems keyboard fsck)/' /etc/mkinitcpio.conf
-mkinitcpio -P
+# Sudo
+pacman -S sudo --noconfirm
+echo '%wheel ALL=(ALL) ALL' >> /etc/sudoers
 
-pacman -S --noconfirm grub efibootmgr
-grub-install --target=x86_64-efi --efi-directory=/boot --bootloader-id=GRUB
-UUID_LUKS=\$(blkid -s UUID -o value ${DISK}2)
-sed -i "s|^GRUB_CMDLINE_LINUX=\"\"|GRUB_CMDLINE_LINUX=\"cryptdevice=UUID=\$UUID_LUKS:cryptlvm root=/dev/vg0/lvroot\"|" /etc/default/grub
+# Paquets supplémentaires
+pacman -Syu --noconfirm
+pacman -S virtualbox virtualbox-host-dkms linux-headers firefox gcc make htop neofetch git base-devel xorg-server xorg-xinit hyprland wayland xorg-xwayland alacritty --noconfirm
+
+# Dossier partagé
+groupadd shared
+usermod -aG shared colleague
+usermod -aG shared son
+chown :shared /shared
+chmod 770 /shared
+
+# GRUB
+pacman -S grub efibootmgr --noconfirm
+echo "GRUB_CMDLINE_LINUX=\\"cryptdevice=${DISK}3:cryptroot root=/dev/mapper/vg0-root\\"" >> /etc/default/grub
+grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
 grub-mkconfig -o /boot/grub/grub.cfg
 
-pacman -S --noconfirm sudo
-useradd -m -G wheel -s /bin/bash papa
-echo "papa:azerty123" | chpasswd
-useradd -m -s /bin/bash fils
-echo "fils:azerty123" | chpasswd
-
-sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
-
-# Installer les paquets minimaux + I3
-pacman -S --noconfirm nano git htop firefox virtualbox i3 dmenu xorg xorg-xinit 
-
-# Config de i3 pour papa
-mkdir -p /home/papa/.config/i3
-cat <<EOCFG > /home/papa/.config/i3/config
-exec --no-startup-id dmenu_run
-EOCFG
-chown -R papa:papa /home/papa/.config
+# Initramfs
+sed -i 's/^HOOKS=(.*)/HOOKS=(base udev autodetect keyboard keymap modconf block encrypt lvm2 filesystems fsck)/' /etc/mkinitcpio.conf
+mkinitcpio -P
 
 EOF
 
-    echo "=== Fin du chroot. Démontage et fermeture LUKS ==="
-    umount -R /mnt
-    cryptsetup close cryptlvm
+# Nettoyage
+umount -R /mnt
+swapoff -a
 
-    echo "Installation terminée ! Vous pouvez redémarrer la VM."
-}
-
-# APPEL DES FONCTIONS
-setup_partitions
-setup_base_system
-setup_config
+echo "Installation terminée ! Redémarrez le système."
